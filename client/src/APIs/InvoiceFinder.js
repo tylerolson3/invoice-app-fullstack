905e3d0ad4e49d26158b7f6da081566ee481ee49
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-02-17 20:11:08 -0600

    i dunno?

--- client/src/APIs/InvoiceFinder.js
+++ client/src/APIs/InvoiceFinder.js
@@ -1,14 +1,22 @@
 import axios from "axios";
-import axiosRetry from "axios-retry";
 
-const InvoiceFinder = axios.create({
-  baseURL:
-    process.env.NODE_ENV !== "production"
-      ? "http://localhost:3001/api/v1/invoices"
-      : "https://alluring-bryce-canyon-75245.herokuapp.com/api/v1/restaurants",
-  timeout: 5000,
+export default axios.create({
+  baseURL: "http://localhost:3030/api/v1/invoices",
 });
 
-axiosRetry(InvoiceFinder, { retries: 3 });
 
-export default InvoiceFinder;
+
+// import axios from "axios";
+// import axiosRetry from "axios-retry";
+
+// const InvoiceFinder = axios.create({
+//   baseURL:
+//     process.env.NODE_ENV !== "production"
+//       ? "http://localhost:3001/api/v1/invoices"
+//       : "https://alluring-bryce-canyon-75245.herokuapp.com/api/v1/restaurants",
+//   timeout: 5000,
+// });
+
+// axiosRetry(InvoiceFinder, { retries: 3 });
+
+// export default InvoiceFinder;
--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,20 @@
-const { Pool } = require("pg");
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const pool = new Pool({
-  connectionString: isProduction
-    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
-    : connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-});
+// const { Pool } = require("pg");
+// const isProduction = process.env.NODE_ENV === "production";
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const pool = new Pool({
+//   connectionString: isProduction
+//     ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+//     : connectionString,
+//   ssl: isProduction ? { rejectUnauthorized: false } : false,
+// });
 
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
+
+
+const { Pool } = require("pg");
+const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
